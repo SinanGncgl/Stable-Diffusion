cc612e362052bd3920b0c4aebd916304490ec092
Sinan Gençoğlu <sinan.gencogluu@gmail.com>
2023-07-01 21:36:57 +0300

    Update download_first_stages.sh

--- scripts/download_first_stages.sh
+++ scripts/download_first_stages.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-wget -O models/first_stage_models/kl-f4/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f4.zip
-wget -O models/first_stage_models/kl-f8/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f8.zip
-wget -O models/first_stage_models/kl-f16/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f16.zip
-wget -O models/first_stage_models/kl-f32/model.zip https://ommer-lab.com/files/latent-diffusion/kl-f32.zip
-wget -O models/first_stage_models/vq-f4/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f4.zip
-wget -O models/first_stage_models/vq-f4-noattn/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f4-noattn.zip
-wget -O models/first_stage_models/vq-f8/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f8.zip
-wget -O models/first_stage_models/vq-f8-n256/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f8-n256.zip
-wget -O models/first_stage_models/vq-f16/model.zip https://ommer-lab.com/files/latent-diffusion/vq-f16.zip
+curl https://ommer-lab.com/files/latent-diffusion/kl-f4.zip --create-dirs -o models_ckpts/first_stage_models/kl-f4/model.zip 
+curl https://ommer-lab.com/files/latent-diffusion/kl-f8.zip --create-dirs -o models_ckpts/first_stage_models/kl-f8/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/kl-f16.zip --create-dirs -o models_ckpts/first_stage_models/kl-f16/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/kl-f32.zip --create-dirs -o models_ckpts/first_stage_models/kl-f32/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/vq-f4.zip --create-dirs -o models_ckpts/first_stage_models/vq-f4/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/vq-f4-noattn.zip --create-dirs -o models_ckpts/first_stage_models/vq-f4-noattn/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/vq-f8.zip --create-dirs -o models_ckpts/first_stage_models/vq-f8/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/vq-f8-n256.zip --create-dirs -o models_ckpts/first_stage_models/vq-f8-n256/model.zip
+curl https://ommer-lab.com/files/latent-diffusion/vq-f16.zip --create-dirs -o models_ckpts/first_stage_models/vq-f16/model.zip
 
 
 
-cd models/first_stage_models/kl-f4
+cd models_ckpts/first_stage_models/kl-f4
 unzip -o model.zip
 
 cd ../kl-f8
